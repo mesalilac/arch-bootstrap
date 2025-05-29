6a7a09a81238ee7eecfd6314d459a7f58d03c60c
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-05-29 19:50:02 +0300

    validate script

--- utils/validate.bash
+++ utils/validate.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-# TODO: read include/packages and check if the packages exists
+# Read include/packages and check if the packages doesn't exists
+
+. "include/packages.bash"
+
+echo "Checking pacman packages"
+
+for pkg in "${PACMAN_PACKAGES[@]}";
+do
+    if ! pacman -Ss "^${pkg}$" > /dev/null ; then
+        echo "ERROR: package '${pkg}' not found!"
+    fi
+done
