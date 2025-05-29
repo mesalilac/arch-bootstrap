3f8537ca2f9c9b58306373a25fe90450ed3adba8
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-05-29 19:49:49 +0300

    add more packages

--- include/packages.bash
+++ include/packages.bash
@@ -1,6 +1,7 @@
 # TODO: add all packages from debian-packages.sh
 # TODO: write a python script to format this file and add package info
 export PACMAN_PACKAGES=(
+    "lua-language-server"
     "wl-clipboard"
     "xdg-desktop-portal-hyprland"
     "hyprpolkitagent"
