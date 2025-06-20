caa4f79e3223744bdee04e96e2d0247d14b820f9
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-06-20 11:22:59 +0300

    power profiles daemon

--- include/packages.bash
+++ include/packages.bash
@@ -1,6 +1,7 @@
 # TODO: add all packages from debian-packages.sh
 # TODO: write a python script to format this file and add package info
 export PACMAN_PACKAGES=(
+    "power-profiles-daemon"
     "eslint_d"
     "typescript-language-server"
     "flatpak"
